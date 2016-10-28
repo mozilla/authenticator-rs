1269f0b17a810953b34f1a3142180113e79aca39
Kyle Machulis <kyle@nonpolynomial.com>
2016-10-28 16:49:55 -0700

    Detect devices, send/recv function

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,186 @@ extern crate libudev;
 #[path="linux/mod.rs"]
 pub mod platform;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::mem;
+use std::io;
+
+const HID_RPT_SIZE : usize = 64;
+const CID_BROADCAST : u32 = 0xffffffff;
+const TYPE_MASK : u8 = 0x80;
+const TYPE_INIT : u8 = 0x80;
+const TYPE_CONT : u8 = 0x80;
+
+const FIDO_USAGE_PAGE     : u16 =    0xf1d0;	// FIDO alliance HID usage page
+const FIDO_USAGE_U2FHID   : u8  =   0x01;	// U2FHID usage for top-level collection
+const FIDO_USAGE_DATA_IN  : u8  =   0x20;	// Raw IN data report
+const FIDO_USAGE_DATA_OUT : u8  =   0x21;	// Raw OUT data report
+
+// General constants
+
+const U2FHID_IF_VERSION    : u32 =  2;	// Current interface implementation version
+const U2FHID_FRAME_TIMEOUT : u32 =  500;	// Default frame timeout in ms
+const U2FHID_TRANS_TIMEOUT : u32 =  3000;	// Default message timeout in ms
+
+// U2FHID native commands
+
+const U2FHID_PING         : u8 = (TYPE_INIT | 0x01);	// Echo data through local processor only
+const U2FHID_MSG          : u8 = (TYPE_INIT | 0x03);	// Send U2F message frame
+const U2FHID_LOCK         : u8 = (TYPE_INIT | 0x04);	// Send lock channel command
+const U2FHID_INIT         : u8 = (TYPE_INIT | 0x06);	// Channel initialization
+const U2FHID_WINK         : u8 = (TYPE_INIT | 0x08);	// Send device identification wink
+const U2FHID_ERROR        : u8 = (TYPE_INIT | 0x3f);	// Error response
+const U2FHID_VENDOR_FIRST : u8 = (TYPE_INIT | 0x40);	// First vendor defined command
+const U2FHID_VENDOR_LAST  : u8 = (TYPE_INIT | 0x7f);	// Last vendor defined command
+
+// U2FHID_INIT command defines
+
+const INIT_NONCE_SIZE     : usize =    8;	// Size of channel initialization challenge
+const CAPFLAG_WINK        : u8 =    0x01;	// Device supports WINK command
+const CAPFLAG_LOCK        : u8 =    0x02;	// Device supports LOCK command
+
+// Low-level error codes. Return as negatives.
+
+const ERR_NONE            : u8 =    0x00;	// No error
+const ERR_INVALID_CMD     : u8 =    0x01;	// Invalid command
+const ERR_INVALID_PAR     : u8 =    0x02;	// Invalid parameter
+const ERR_INVALID_LEN     : u8 =    0x03;	// Invalid message length
+const ERR_INVALID_SEQ     : u8 =    0x04;	// Invalid message sequencing
+const ERR_MSG_TIMEOUT     : u8 =    0x05;	// Message has timed out
+const ERR_CHANNEL_BUSY    : u8 =    0x06;	// Channel busy
+const ERR_LOCK_REQUIRED   : u8 =    0x0a;	// Command requires channel lock
+const ERR_INVALID_CID     : u8 =    0x0b;	// Command not allowed on this cid
+const ERR_OTHER           : u8 =    0x7f;	// Other unspecified error
+
+#[repr(packed)]
+struct U2FHIDInitReq {
+    nonce: [u8; INIT_NONCE_SIZE]
+}
+
+#[repr(packed)]
+struct U2FHIDInitResp {
+    nonce: [u8; INIT_NONCE_SIZE],
+    cid: u32,
+    version_interface: u8,
+    version_major: u8,
+    version_minor: u8,
+    version_build: u8,
+    cap_flags: u8
+}
+
+#[repr(packed)]
+struct U2FHIDInit {
+    cid: u32,
+    cmd: u8,
+    bcnth: u8,
+    bcntl: u8,
+    data: [u8; HID_RPT_SIZE - 7]
+}
+
+#[repr(packed)]
+struct U2FHIDCont {
+    cid: u32,
+    seq: u8,
+    data: [u8; HID_RPT_SIZE - 5]
+}
+
+pub fn init_device(dev: &mut platform::Device) {
+    println!("Initing device!");
+    let nonce = vec![0x8, 0x7, 0x6, 0x5, 0x4, 0x3, 0x2, 0x1];
+    let mut recv : Vec<u8> = Vec::new();
+    sendrecv(dev, U2FHID_INIT, &nonce, &mut recv);
+}
+
+pub fn sendrecv(dev: &mut platform::Device, cmd: u8, send: &Vec<u8>, recv: &mut Vec<u8>) -> io::Result<()> {
+    let mut data_sent: usize = 0;
+    let mut sequence: u8 = 0;
+
+    println!("Sending data!");
+    // Write Data.
+    while send.len() > data_sent {
+        let frame : [u8; HID_RPT_SIZE];
+
+        if data_sent == 0 {
+            let mut uf = U2FHIDInit {
+                cid: dev.cid,
+                cmd: cmd,
+                bcnth: (send.len() >> 8) as u8,
+                bcntl: send.len() as u8,
+                data: [0; HID_RPT_SIZE - 7]
+            };
+            // clone_from_slice panics if src/dst sizes don't match.
+            uf.data[0..send.len()].clone_from_slice(send);
+            unsafe {
+                frame = mem::transmute(uf);
+            }
+        } else {
+            let uf = U2FHIDCont {
+                cid: dev.cid,
+                seq: sequence,
+                data: [0; HID_RPT_SIZE - 5]
+            }
+            uf.data[0..send.len()].clone_from_slice(send);
+            unsafe {
+                frame = mem::transmute(uf);
+            }
+        }
+
+        sequence += 1;
+        // TODO Figure out nicer way to prepend record number
+        let mut frame_data : [u8; HID_RPT_SIZE + 1] = [0u8; HID_RPT_SIZE + 1];
+        frame_data[0] = 0;
+        frame_data[1..].clone_from_slice(&frame);
+
+        match platform::write(dev, &frame_data) {
+            Ok(n) => data_sent += n,
+            Err(er) => {
+                println!("Write fucked up! {:?}", er);
+                return Err(io::Error::new(io::ErrorKind::Other, "WRITING FUCKED UP"));
+            }
+        };
+        println!("Sent data!");
+        // TODO Take care of result
     }
+    println!("Receiving data!");
+    // Now we read. This happens in 2 chunks: The initial packet, which has the
+    // size we expect overall, then continuation packets, which will fill in
+    // data until we have everything.
+    let mut raw_frame : [u8; HID_RPT_SIZE] = [0u8; HID_RPT_SIZE];
+
+    // TODO Check the status of the read, figure out how we'll deal with timeouts.
+    let recvlen = platform::read(dev, &mut raw_frame).unwrap();
+    println!("Read! {:?}", recvlen);
+    for x in 0..64 {
+        println!("{:?}", raw_frame[x]);
+    }
+    // We'll get an init packet back from USB, open it to see how much we'll be
+    // reading overall. (should unpack to an init struct)
+    let mut info_frame : U2FHIDInit;
+    unsafe {
+        info_frame = mem::transmute(raw_frame);
+    }
+
+    // Read until we've exhausted the total read amount or error out
+    let datalen : usize = (info_frame.bcnth as usize) << 8 | (info_frame.bcntl as usize);
+    //let recvlen : u16 = 0;
+    sequence = 0;
+    println!("Expecting data length: {:?}", datalen);
+    println!("Receiving even more data!");
+    while recvlen < datalen {
+        let mut frame : [u8; HID_RPT_SIZE] = [0u8; HID_RPT_SIZE];
+        println!("Got more data! {:?}", platform::read(dev, &mut frame));
+
+        let mut cont_frame : U2FHIDCont;
+        unsafe {
+            cont_frame = mem::transmute(frame);
+        }
+        if cont_frame.seq != sequence + 1 {
+            println!("Error in sequence number!");
+            // TODO: This should be an error.
+            return Ok(());
+        }
+        sequence = cont_frame.seq;
+    }
+    println!("Finished receiving data!");
+    // Return the read data buffer.
+    Ok(())
 }
--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -1,7 +1,10 @@
 use libudev;
-use std::fs::File;
+use std::path::Path;
+use std::fs::{File, OpenOptions};
 use std::os::unix::io::AsRawFd;
 use std::io;
+use std::io::{Read, Write};
+use ::{init_device, CID_BROADCAST, U2FHIDInitResp};
 
 #[allow(non_camel_case_types)]
 #[repr(C)]
@@ -10,22 +13,30 @@ pub struct hidraw_report_descriptor {
     pub value: [u8;4096]
 }
 
-#[allow(non_camel_case_types)]
-#[derive(Debug)]
-#[repr(C)]
-struct hidraw_devinfo {
-    __bustype: u32,
-    __vendor: i16,
-    __product: i16
-}
-
+// Taken from ioctl crate, but it doesn't look like they're alive anymore?
 ioctl!(read hidiocgrdescsize with b'H', 0x01; ::libc::c_int);
 ioctl!(read hidiocgrdesc with b'H', 0x02; /*struct*/ hidraw_report_descriptor);
 
-struct Device {
-    device_handle: i32,
-    blocking: bool,
-    uses_numbered_reports: bool
+pub struct Device {
+    pub device: File,
+    pub blocking: bool,
+    pub uses_numbered_reports: bool,
+    pub cid: u32,
+}
+
+#[derive(Debug)]
+struct DeviceUsage {
+    pub usage: u16,
+    pub usage_page: u16
+}
+
+impl DeviceUsage {
+    pub fn new() -> DeviceUsage {
+        DeviceUsage {
+            usage: 0,
+            usage_page: 0
+        }
+    }
 }
 
 impl hidraw_report_descriptor {
@@ -48,6 +59,74 @@ fn from_nix_result<T>(res: ::nix::Result<T>) -> io::Result<T> {
     }
 }
 
+fn get_bytes(desc: &[u8], num_bytes: usize, cur: usize) -> u32 {
+    /* Return if there aren't enough bytes. */
+    if cur + num_bytes >= desc.len() {
+        return 0;
+    }
+
+    if num_bytes == 0 {
+        return 0;
+    }
+    else if num_bytes == 1 {
+        return desc[cur + 1] as u32;
+    }
+    else if num_bytes == 2 {
+        return ((desc[cur + 2] as u32) << 8) + (desc[cur + 1] as u32);
+    }
+    0
+}
+
+fn get_usage(desc: &[u8]) -> Result<DeviceUsage, ()> {
+    let mut du : DeviceUsage = DeviceUsage::new();
+    let mut size_code: u8 = 0;
+    let mut data_len: usize = 0;
+    let mut key_size: i32 = 0;
+    let mut usage_found: bool = false;
+    let mut usage_page_found: bool = false;
+    let mut i: usize = 0;
+    while i < desc.len() {
+        let key = desc[i];
+        let key_cmd = key & 0xfc;
+        data_len = 0;
+        if key & 0xf0 == 0xf0 {
+            return Err(());
+        }
+        size_code = (key & 0x3) as u8;
+        match size_code
+	      {
+	          0 => data_len = size_code as usize,
+            1 => data_len = size_code as usize,
+            2 => data_len = size_code as usize,
+            3 => data_len = 4,
+            default => data_len = 0
+	      };
+        key_size = 1;
+        println!("start: {0}, len: {1}", i, data_len);
+        if key_cmd == 0x4
+        {
+            du.usage_page = get_bytes(desc, data_len, i) as u16;
+            usage_page_found = true;
+        }
+        if key_cmd == 0x8
+        {
+            du.usage = get_bytes(desc, data_len, i) as u16;
+            usage_found = true;
+        }
+
+        if usage_page_found && usage_found {
+            // TODO: Hack to make sure we only access Yubikey
+            if du.usage == 1 {
+                return Ok(du);		/* success */
+            }
+            return Err(());
+        }
+        i += data_len + (key_size as usize);
+    }
+
+    return Err(())
+}
+
 fn get_usages(dev : &libudev::Device) -> io::Result<()> {
     let mut desc_size: i32 = 0;
     let fd;
@@ -62,12 +141,37 @@ fn get_usages(dev : &libudev::Device) -> io::Result<()> {
         Ok(_) => println!("Descriptor size: {:?}", desc_size),
         Err(e) => return Err(io::Error::new(io::ErrorKind::Other, "Can't get descriptor!"))
     }
+    println!("{:?} {:?}", dev.devnode(), dev.sysname());
     let mut rpt_desc: hidraw_report_descriptor = hidraw_report_descriptor::new(desc_size as u32);
     match from_nix_result(unsafe { hidiocgrdesc(fd, &mut rpt_desc) }) {
         Ok(_) => println!("Descriptor size: {:?}", desc_size),
         Err(e) => return Err(io::Error::new(io::ErrorKind::Other, "Can't get descriptor!"))
     }
-    Ok(())
+    match get_usage(&rpt_desc.value[0..(desc_size as usize)]) {
+        Ok(_) => Ok(()),
+        Err(er) => return Err(io::Error::new(io::ErrorKind::Other, "Can't get descriptor!"))
+    }
+}
+
+pub fn open_device(fname: &Path) -> io::Result<Device> {
+    println!("opening device!");
+    let d = match OpenOptions::new().write(true).read(true).open(fname) {
+        Ok(f) => f,
+        Err(er) => { println!("{:?}", er); return Err(io::Error::new(io::ErrorKind::Other, "Can't open!")); }
+    };
+    println!("Opened device! {:?}", d);
+    let mut device = Device {
+        device: d,
+        blocking: true,
+        // TODO Actually check the usage report here
+        uses_numbered_reports: true,
+        // Start device with CID_BROADCAST as a cid, we'll get the actual CID on
+        // device init.
+        cid: CID_BROADCAST
+    };
+    println!("Trying to init device!");
+    init_device(&mut device);
+    Ok(device)
 }
 
 pub fn find_keys() -> io::Result<()> {
@@ -75,12 +179,27 @@ pub fn find_keys() -> io::Result<()> {
     let mut enumerator = try!(libudev::Enumerator::new(&context));
 
     for device in try!(enumerator.scan_devices()) {
-        let u;
+        //let u;
         match get_usages(&device) {
-            Ok(usage) => u = usage,
+            Ok(usage) => {
+                println!("{:?}", usage);
+                match device.devnode() {
+                    Some(n) => { open_device(n); },
+                    None => println!("Can't open!")
+                };
+            },
             Err(_) => continue
-        }
+        };
     }
 
     Ok(())
+}
+
+pub fn write(dev: &mut Device, bytes: &[u8]) -> io::Result<usize> {
+    println!("writing to device! {:?}", dev.device);
+    dev.device.write(bytes)
+}
+
+pub fn read(dev: &mut Device, bytes: &mut [u8]) -> io::Result<usize> {
+    dev.device.read(bytes)
 }
