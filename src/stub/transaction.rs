07d08df460ef5126e68c20975fa04056d0c3e7ee
J.C. Jones <jc@mozilla.com>
2020-09-01 21:50:14 -0700

    Update all other platforms with the new error mechanism

--- src/stub/transaction.rs
+++ src/stub/transaction.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use crate::errors;
 use crate::statecallback::StateCallback;
 
 pub struct Transaction {}
@@ -9,14 +10,19 @@ pub struct Transaction {}
 impl Transaction {
     pub fn new<F, T>(
         timeout: u64,
-        callback: StateCallback<Result<T, crate::Error>>,
+        callback: StateCallback<crate::Result<T>>,
         new_device_cb: F,
-    ) -> Result<Self, crate::Error>
+    ) -> crate::Result<Self>
     where
         F: Fn(String, &dyn Fn() -> bool),
     {
-        callback.call(Err(crate::Error::NotSupported));
-        Err(crate::Error::NotSupported)
+        callback.call(Err(errors::AuthenticatorError::U2FToken(
+            errors::U2FTokenError::NotSupported,
+        )));
+
+        Err(errors::AuthenticatorError::U2FToken(
+            errors::U2FTokenError::NotSupported,
+        ))
     }
 
     pub fn cancel(&mut self) {
