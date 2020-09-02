75179d6cfb23a18db8cf88f6dcfacf39ead62651
J.C. Jones <jc@mozilla.com>
2020-09-01 21:50:14 -0700

    Update all other platforms with the new error mechanism

--- src/windows/transaction.rs
+++ src/windows/transaction.rs
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use crate::errors;
 use crate::platform::monitor::Monitor;
 use crate::statecallback::StateCallback;
 use runloop::RunLoop;
@@ -14,9 +15,9 @@ pub struct Transaction {
 impl Transaction {
     pub fn new<F, T>(
         timeout: u64,
-        callback: StateCallback<Result<T, crate::Error>>,
+        callback: StateCallback<crate::Result<T>>,
         new_device_cb: F,
-    ) -> Result<Self, crate::Error>
+    ) -> crate::Result<Self>
     where
         F: Fn(String, &dyn Fn() -> bool) + Sync + Send + 'static,
         T: 'static,
@@ -28,14 +29,16 @@ impl Transaction {
 
                 // Start polling for new devices.
                 try_or!(monitor.run(alive), |_| callback
-                    .call(Err(crate::Error::Unknown)));
+                    .call(Err(errors::AuthenticatorError::Platform)));
 
                 // Send an error, if the callback wasn't called already.
-                callback.call(Err(crate::Error::NotAllowed));
+                callback.call(Err(errors::AuthenticatorError::U2FToken(
+                    errors::U2FTokenError::NotAllowed,
+                )));
             },
             timeout,
         )
-        .map_err(|_| crate::Error::Unknown)?;
+        .map_err(|_| errors::AuthenticatorError::Platform)?;
 
         Ok(Self {
             thread: Some(thread),
