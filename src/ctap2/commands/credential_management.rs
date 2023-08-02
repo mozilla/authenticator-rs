29c0b81cb759e40b36b1da0baa35beda52a4f68b
Martin Sirringhaus <>
2023-08-02 15:30:45 +0200

    Try to fix clippy warnings

--- src/ctap2/commands/credential_management.rs
+++ src/ctap2/commands/credential_management.rs
@@ -430,7 +430,7 @@ impl PinUvAuthCommand for CredentialManagement {
             if params.has_some() {
                 data.extend(to_vec(&params).map_err(CommandError::Serializing)?);
             }
-            param = Some(token.clone().derive(&data).map_err(CommandError::Crypto)?);
+            param = Some(token.derive(&data).map_err(CommandError::Crypto)?);
         }
         self.pin_uv_auth_param = param;
         Ok(())
