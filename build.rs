15b5f96cd0122e8ad9e235e4210b297921e18bbd
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2025-05-12 09:10:04 +0200

    Add build support and ioctl bindings for sparc64

--- build.rs
+++ build.rs
@@ -49,6 +49,8 @@ fn main() {
         "ioctl_riscv64.rs"
     } else if cfg!(all(target_arch = "loongarch64", target_endian = "little")) {
         "ioctl_loongarch64.rs"
+    } else if cfg!(all(target_arch = "sparc64", target_endian = "big")) {
+        "ioctl_sparc64.rs"
     } else {
         panic!("architecture not supported");
     };
